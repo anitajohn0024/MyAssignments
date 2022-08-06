f68f2fa12e27f27265641efdc4cbda6627085ee3
anitajohn0024 <110720909+anitajohn0024@users.noreply.github.com>
2022-08-06 17:37:29 +0300

    Add files via upload

---
+++ day1/Car.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Car {
+ byte modelYear = 10;
+ short seatingCapacity = 7;
+ int onRoadPrice = 3600000;
+ long chassisNumber = 543245666543L;
+ float fuelCapacity = 6.0F;
+ double kilometer = 200000.3344555666;
+ char model = 'S';
+ boolean withABSBrake = true;
+ String carName = "Toyota LandCruiser";
+}
